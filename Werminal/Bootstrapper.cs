f5ad24a0dcd979b5f6a6249cc9555fb819b56487
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-05-14 13:04:07 -0700

    Initial Commit

---
+++ Werminal/Bootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Prism.UnityExtensions;
+
+namespace Werminal
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }     
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            var moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(WerminalModule.WerminalModule));
+        }
+    }
+}
---
+++ WerminalModule/WerminalModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace WerminalModule
+{
+    public class WerminalModule : IModule
+    {
+        private readonly IRegionViewRegistry _regionViewRegistry;
+
+        public WerminalModule(IRegionViewRegistry registry)
+        {
+            this._regionViewRegistry = registry;   
+        }
+
+        public void Initialize()
+        {
+            _regionViewRegistry.RegisterViewWithRegion("MainRegion", typeof(Views.WerminalView));
+        }
+    }
+}
