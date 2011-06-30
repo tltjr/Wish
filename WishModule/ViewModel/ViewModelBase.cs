881f62ee65abb0d5b58d8e085ddaeb6679cf7f69
Thomas <thomas.thornton.jr@gmail.com>
2011-06-30 16:55:09 -0700

    current status of repo after heavy source control usage

---
+++ WishModule/ViewModel/ViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Wish.ViewModel
+{
+    public class ViewModelBase
+    {
+        // In ViewModelBase.cs
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.VerifyPropertyName(propertyName);
+
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(string propertyName)
+        {
+            // Verify that the property name matches a real,  
+            // public, instance property on this object.
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                string msg = "Invalid property name: " + propertyName;
+                Debug.Fail(msg);
+            }
+        }
+    }
+}
---
+++ WishModule/ViewModel/WishViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using ICSharpCode.AvalonEdit.Highlighting;
+
+namespace Wish.ViewModel
+{
+    public class WishViewModel : ViewModelBase
+    {
+        private WishModel _wish;
+        private string _title;
+        private string _text;
+        private IHighlightingDefinition _syntaxHighlighting;
+
+        public WishViewModel()
+        {
+            _wish = new WishModel();
+            _syntaxHighlighting = _wish.SyntaxHighlighting
+        }
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (_title == value) return;
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text == value) return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public IHighlightingDefinition SyntaxHighlighting
+        {
+            get { return _syntaxHighlighting; }
+            set
+            {
+                if (_syntaxHighlighting == value) return;
+                _syntaxHighlighting = value;
+                OnPropertyChanged("SyntaxHighlighting");
+            }
+        }
+
+        public void KeyPress(KeyEventArgs keyEventArgs)
+        {
+            _wish.KeyPress(keyEventArgs);
+        }
+    }
+}
