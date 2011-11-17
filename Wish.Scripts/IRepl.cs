604ee76c82a3f55a6b96ccce80050e40045e3ea7
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-11-17 15:40:06 -0700

    playing with xml comments

--- Wish.Scripts/IRepl.cs
+++ Wish.Scripts/IRepl.cs
@@ -11,6 +11,11 @@ namespace Wish.Scripts
         string Print();
         CommandResult Loop(IRunner runner, string text);
         CommandResult Loop(string text);
+        /// <summary>
+        /// Initilizes the repl, inserts a prompt and
+        /// changes to the appropriate directory.
+        /// </summary>
+        /// <returns>A CommandResult with the text and starting working directory.</returns>
         CommandResult Start();
         CommandResult Up(string text);
         CommandResult Down(string text);
