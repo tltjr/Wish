3f8de2d02feaa43608d16a4cc6509f143e0fcd59
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-11-19 12:10:07 -0700

    view model dictionary tests

---
+++ Wish.SearchBox.Tests/ViewModelDictionaryTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Wish.SearchBox.ViewModels;
+
+namespace Wish.SearchBox.Tests
+{
+    [TestFixture]
+    public class ViewModelDictionaryTests
+    {
+        private ViewModelDictionary _vmDict;
+
+        [SetUp]
+        public void Init()
+        {
+            _vmDict = new ViewModelDictionary();
+        }
+
+        [Test]
+        public void CommandHistory()
+        {
+            var vm = _vmDict[SearchType.CommandHistory];
+            Assert.IsInstanceOf<CommandHistoryViewModel>(vm);
+        }
+
+        [Test]
+        public void RecentDirectories()
+        {
+            var vm = _vmDict[SearchType.RecentDirectories];
+            Assert.IsInstanceOf<RecentDirectoriesViewModel>(vm);
+        }
+
+        [Test]
+        public void RecentArguments()
+        {
+            var vm = _vmDict[SearchType.RecentArguments];
+            Assert.IsInstanceOf<RecentArgumentsViewModel>(vm);
+        }
+    }
+}
