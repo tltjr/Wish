efc6c18018466604a9f2392b0c1ea4cfacc85718
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-11-19 07:59:03 -0700

    added search box vm tests and removed avalon edit tests from solution

---
+++ Wish.SearchBox.Tests/CommandHistoryViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Wish.SearchBox.ViewModels;
+
+namespace Wish.SearchBox.Tests
+{
+    [TestFixture]
+    public class CommandHistoryViewModelTests
+    {
+        private CommandHistoryViewModel _chvm;
+
+        [SetUp]
+        public void Init()
+        {
+            _chvm = new CommandHistoryViewModel();
+        }
+
+        [Test]
+        public void Color()
+        {
+            Assert.AreEqual("#7E51FD", _chvm.Color);
+        }
+
+        [Test]
+        public void Text()
+        {
+            Assert.AreEqual("Search Command History: ", _chvm.Text);
+        }
+    }
+}
---
+++ Wish.SearchBox.Tests/RecentDirectoriesViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Wish.SearchBox.ViewModels;
+
+namespace Wish.SearchBox.Tests
+{
+    [TestFixture]
+    public class RecentDirectoriesViewModelTests
+    {
+        private RecentDirectoriesViewModel _rdvm;
+
+        [SetUp]
+        public void Init()
+        {
+            _rdvm = new RecentDirectoriesViewModel();
+        }
+
+        [Test]
+        public void Color()
+        {
+            Assert.AreEqual("#ffc853", _rdvm.Color);
+        }
+
+        [Test]
+        public void Text()
+        {
+            Assert.AreEqual("Search Recent Directories: ", _rdvm.Text);
+        }
+    }
+}
