11369cc4adcdc931fb5921937798c7a8f8ff338e
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-11-29 15:33:40 -0700

    attempt to read _wishrc from home dir

--- Wish.Scripts/Repl.cs
+++ Wish.Scripts/Repl.cs
@@ -49,10 +49,29 @@ namespace Wish.Scripts
             var command = new Command("cd " + _prompt.WorkingDirectory);
             command.Execute();
             var pshell = new Powershell();
-            var blah = pshell.Execute(File.ReadAllText(@"C:\temp\prof.ps1"));
+            var home = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            string profileInfo = null;
+            if(!string.IsNullOrEmpty(home))
+            {
+                var profile = Path.Combine(home, "_wishrc");
+                var fi = new FileInfo(profile);
+                if (fi.Exists)
+                {
+                    profileInfo = pshell.Execute(File.ReadAllText(profile));
+                }
+            }
+            string tempText;
+            if(string.IsNullOrWhiteSpace(profileInfo))
+            {
+                tempText = Text;
+            }
+            else
+            {
+                tempText = profileInfo + "\n\n" + Text;
+            }
             return new CommandResult
                        {
-                           Text = Text,
+                           Text = tempText,
                            WorkingDirectory = _prompt.WorkingDirectory,
                            PromptLength = _prompt.Current.Length + 1,
                            FullyProcessed = false
